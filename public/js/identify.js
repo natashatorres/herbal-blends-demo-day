ddf97e634edc67d883abaf94d974094de5e63662
Natasha Torres <natashaxtorres@gmail.com>
2021-12-09 20:53:40 -0500

    updates 12/6/21

--- public/js/identify.js
+++ public/js/identify.js
@@ -2,15 +2,25 @@ let camera_button = document.querySelector("#start-camera");
 let video = document.querySelector("#video");
 let click_button = document.querySelector("#click-photo");
 let canvas = document.querySelector("#canvas");
+let identification = document.getElementById('identification')
+
 
 camera_button.addEventListener('click', async function () {
     let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
     video.srcObject = stream;
-
+    video.classList.remove('hidden')
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    identification.classList.add('hidden')
+  
 });
 
 //fetch within the capture image button
 click_button.addEventListener('click', function () {
+    
+    video.classList.add('hidden')
+    identification.classList.remove('hidden')
+
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
     let image_data_url = canvas.toDataURL('image/jpeg');
     canvas.toBlob(function (blob) {
